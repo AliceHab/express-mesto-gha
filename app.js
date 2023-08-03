69c48ab3fbcb5656ed5b2b73976d3cf308e02586
Maxim Novak <maksim.efremof@gmail.com>
2023-08-03 21:53:26 +0300

    fix: исправлена ошибка теста

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().email.required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri('http', 'https'),
+      avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
     }),
   }),
   createUser,
